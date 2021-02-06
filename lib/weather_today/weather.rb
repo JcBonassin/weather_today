b1eeac0dabf8efb5c8a3756e02e38b3b83b44008
JC <bonassin@gmail.com>
2021-02-05 18:40:01 -0600

    new update

--- lib/weather_today/weather.rb
+++ lib/weather_today/weather.rb
@@ -1,21 +1,23 @@
 class WeatherToday::Weather
+
     attr_accessor :location, :date, :temp, :decription
+
 
     def self.today
         #return all the data from the API
-        puts <<-DOC
-            my location
-            date
-            temp
-            decription
-        DOC
-    end 
+        #puts <<-DOC
+        #    my location
+        #    date
+        #    temp
+        #    decription
+        #DOC
 
-    #weather_today = self.new
-    #weather_today.location = "my location"
-    #weather_today.date = "today's date"
-    #weather_today.temp = "temperature"
-    #weather_today.decription = "descrription"
-    
-    #[ weather_today]
+        weather_today = self.new
+        weather_today.location = "location"
+        weather_today.date = "date"
+        weather_today.temp = "temp"
+        weather_today.decription = "description"
+
+     [weather_today]
+    end 
 end 
